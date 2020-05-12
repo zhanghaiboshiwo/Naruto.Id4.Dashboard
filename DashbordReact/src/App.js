0b1bf3ec11332cdb88525bee827c55ee606bb96f
海波 <1091675746@qq.com>
2020-05-12 18:16:51 +0800

    提交react布局

--- DashbordReact/src/App.js
+++ DashbordReact/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+        <Layout>
+            <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+              <div className="logo" />
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['client']}>
+                <Menu.Item key="client">客户端</Menu.Item>
+                <Menu.Item key="resource">资源</Menu.Item>
+              </Menu>
+            </Header>
+            <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+              </Breadcrumb>
+              <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+               
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
+    );
+  }
 }
 
 export default App;
