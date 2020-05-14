2e953663cfd57fb737e4b2e22dada1f47ce81b4a
海波 <1091675746@qq.com>
2020-05-14 17:57:17 +0800

    页面更改

--- DashbordReact/src/client/add.js
+++ DashbordReact/src/client/add.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Drawer, Form, Button, Col, Row, Input, Select, DatePicker } from 'antd';
+import { Drawer, Form, Button, Col, Row, Input, Select ,InputNumber } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
@@ -9,7 +9,9 @@ export default class AddClient extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { visible: false };
+    this.state = { visible: false ,
+      childrenDrawer:false
+    };
 }
   showDrawer = () => {
     this.setState({
@@ -23,6 +25,16 @@ export default class AddClient extends React.Component {
     });
   };
 
+  addGrantTypeEvent=()=>{
+    this.setState({
+      childrenDrawer: true,
+    });
+  }
+  onGrantTypeDrawerClose=()=>{
+    this.setState({
+      childrenDrawer: false,
+    });
+  }
   render() {
     return (
       <>
@@ -31,7 +43,7 @@ export default class AddClient extends React.Component {
         </Button>
         <Drawer
           title="新增客户端"
-          width={document.body.clientWidth-150}
+          width={650}
           onClose={this.onClose}
           visible={this.state.visible}
           bodyStyle={{ paddingBottom: 80 }}
@@ -52,102 +64,172 @@ export default class AddClient extends React.Component {
           }
         >
           <Form layout="vertical" hideRequiredMark>
-            <Row gutter={16}>
-              <Col span={12}>
+            <Row gutter={24}>
+              <Col span={8}>
                 <Form.Item
-                  name="name"
-                  label="Name"
-                  rules={[{ required: true, message: 'Please enter user name' }]}
-                >
-                  <Input placeholder="Please enter user name" />
+                  name="ClientId"
+                  label="客户端Id"
+                  rules={[{ required: true, message: '请输入客户端Id' }]}>
+                <Input placeholder="请输入客户端Id" />
                 </Form.Item>
               </Col>
-              <Col span={12}>
-                <Form.Item
-                  name="url"
-                  label="Url"
-                  rules={[{ required: true, message: 'Please enter url' }]}
-                >
-                  <Input
-                    style={{ width: '100%' }}
-                    addonBefore="http://"
-                    addonAfter=".com"
-                    placeholder="Please enter url"
-                  />
-                </Form.Item>
+              <Col span={11} offset={5}>
+              <Form.Item
+                  name="GrantType"
+                  label="授权类型">
+              <Button type="primary"  size="large" onClick={this.addGrantTypeEvent}>
+              <PlusOutlined />新增 
+              </Button>
+              <ShowDrawer childrenDrawer={this.state.childrenDrawer} title="授权类型" width={500} onChildrenDrawerClose={this.onGrantTypeDrawerClose}/>
+              </Form.Item>
               </Col>
             </Row>
-            <Row gutter={16}>
-              <Col span={12}>
+            <Row gutter={24}>
+              <Col span={8}>
                 <Form.Item
-                  name="owner"
-                  label="Owner"
-                  rules={[{ required: true, message: 'Please select an owner' }]}
-                >
-                  <Select placeholder="Please select an owner">
-                    <Option value="xiao">Xiaoxiao Fu</Option>
-                    <Option value="mao">Maomao Zhou</Option>
-                  </Select>
+                  name="ClientName"
+                  label="客户端名称"
+                  rules={[{ required: true, message: '请输入客户端名称' }]}>
+                <Input placeholder="请输入客户端名称" />
                 </Form.Item>
               </Col>
-              <Col span={12}>
+              <Col span={11} offset={5}>
+              <Form.Item
+                  name="Scope"
+                  label="授权范围">
+              <Button type="primary"  size="large">
+              <PlusOutlined />新增 
+              </Button>
+              </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+             <Col span={8}>
+               <Form.Item
+                 name="Description"
+                 label="客户端描述"
+                 rules={[{  message: '请输入客户端描述' }]}>
+               <Input placeholder="请输入客户端描述" />
+               </Form.Item>
+             </Col>
+             <Col span={11} offset={5}>
+              <Form.Item
+                  name="Origin"
+                  label="跨域来源">
+              <Button type="primary"  size="large">
+              <PlusOutlined />新增 
+              </Button>
+              </Form.Item>
+              </Col>
+           </Row>
+            <Row gutter={24}>
+              <Col span={8}>
                 <Form.Item
-                  name="type"
-                  label="Type"
-                  rules={[{ required: true, message: 'Please choose the type' }]}
-                >
+                  name="ProtocolType"
+                  label="协议类型"
+                  rules={[{ required: true, message: '' }]}>
+                   <Input />
+                </Form.Item>
+              </Col>
+              <Col span={11} offset={5}>
+              <Form.Item
+                  name="Redirect"
+                  label="重定向Uri">
+              <Button type="primary"  size="large">
+              <PlusOutlined />新增 
+              </Button>
+              </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col span={8}>
+                <Form.Item
+                  name="ClientSecretsType"
+                  label="加密方式">
                   <Select placeholder="Please choose the type">
-                    <Option value="private">Private</Option>
-                    <Option value="public">Public</Option>
+                    <Option value="SharedSecret">SharedSecret</Option>
                   </Select>
                 </Form.Item>
               </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={12}>
-                <Form.Item
-                  name="approver"
-                  label="Approver"
-                  rules={[{ required: true, message: 'Please choose the approver' }]}
-                >
-                  <Select placeholder="Please choose the approver">
-                    <Option value="jack">Jack Ma</Option>
-                    <Option value="tom">Tom Liu</Option>
-                  </Select>
-                </Form.Item>
+              <Col span={11} offset={5}>
+              <Form.Item
+                  name="PostOut"
+                  label="注销后重定向Uri">
+              <Button type="primary"  size="large">
+              <PlusOutlined />新增 
+              </Button>
+              </Form.Item>
               </Col>
-              <Col span={12}>
+            </Row>
+            <Row gutter={24}>
+              <Col span={8}>
                 <Form.Item
-                  name="dateTime"
-                  label="DateTime"
-                  rules={[{ required: true, message: 'Please choose the dateTime' }]}
-                >
-                  <DatePicker.RangePicker
-                    style={{ width: '100%' }}
-                    getPopupContainer={trigger => trigger.parentNode}
-                  />
+                  name="ClientSecretsValue"
+                  label="秘钥"
+                  rules={[{ required: true, message: '' }]}>
+                   <Input />
                 </Form.Item>
               </Col>
             </Row>
-            <Row gutter={16}>
-              <Col span={24}>
+            <Row gutter={24}>
+              <Col span={8}>
                 <Form.Item
-                  name="description"
-                  label="Description"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'please enter url description',
-                    },
-                  ]}
-                >
-                  <Input.TextArea rows={4} placeholder="please enter url description" />
+                  name="IdentityTokenLifetime"
+                  label="身份周期"
+                  rules={[{ required: true, message: 'Please choose the approver' }]}>
+                <InputNumber min={1} defaultValue={3600}  placeholder="单位秒"/>
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col span={8}>
+                <Form.Item
+                  name="AccessTokenLifetime"
+                  label="访问周期"
+                  rules={[{ required: true, message: 'Please choose the dateTime' }]}>
+                 <InputNumber min={1} defaultValue={3600} placeholder="单位秒"/>
                 </Form.Item>
               </Col>
             </Row>
           </Form>
         </Drawer>
       </>
+    );
+  }
+}
+
+class ShowDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+
+  }
+
+  render() { 
+    return (  
+      <Drawer
+      title={this.props.title}
+      width={this.props.width}
+      closable={true}
+      maskClosable={false} 
+      onClose={this.props.onChildrenDrawerClose}
+      footer={
+        <div
+          style={{
+            textAlign: 'right',
+          }}
+        >
+          <Button onClick={this.onChildrenDrawerClose} style={{ marginRight: 8 }}>
+            Cancel
+          </Button>
+          <Button onClick={this.onChildrenDrawerClose} type="primary">
+            Submit
+          </Button>
+        </div>
+      }
+      visible={this.props.childrenDrawer}>
+      {this.props.Content}
+    </Drawer>
     );
   }
 }
